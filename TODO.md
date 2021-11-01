f1de069d03be17ed70bce505ea8aacdc7890e757
implicitdef <implicitdef@users.noreply.github.com>
2021-11-01 22:51:03 +0100

    fix redis usage

--- TODO.md
+++ TODO.md
@@ -1,7 +1,6 @@
-Fix le script qui p
+vérifier que tout va bien et redéploy
 
 Fixer les liens vers les ingrédients
-
 Rebosser pour que les cocktails disparaissent pas de la recherche quand on change leur ingrédients
 
 Revoir/comlpéter le crawling du site pour vraiment choper tous les cocktails (et ne plus dépendre des cocktails hardcodés)
--- fetch_script/redisCache.js
+++ fetch_script/redisCache.js
@@ -4,11 +4,10 @@ const prefix = "cocktailsdb";
 
 console.log("Connecting to redis");
 const client = await createClient({
-  hostname: "127.0.0.1",
-  port: 6379,
+  socket: { host: "127.0.0.1", port: 6379 },
 });
 client.on("error", (err) => console.log("Redis Client Error", err));
-console.log("Connected.");
+await client.connect();
 
 export function redisSet(key, value) {
   return client.set(`${prefix}:${key}`, value);
@@ -16,12 +15,7 @@ export function redisSet(key, value) {
 
 export async function redisGet(key) {
   const res = await client.get(`${prefix}:${key}`);
-  if (
-    !res ||
-    // j'ai eu un bug où je recevais un boolean "true", je sais pas pk ??
-    typeof res != "string"
-  )
-    return null;
+  if (!res) return null;
   return res;
 }
 
